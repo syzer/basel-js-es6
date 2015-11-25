6b604e802775cf8ed9625eb09ebbc7f0af7b35f0
syzer3@gmail.com <syzer3@gmail.com>
2015-11-25 20:01:24 +0100

    not so old people

---
+++ NotSoOldPeople.js
@@ -0,0 +1,27 @@
+module.exports = function(people = []) {
+
+    return {
+        hasName,
+        findName,
+        allNames,
+        firstPersonCard
+    };
+
+    function hasName(name) {
+        return !!people.filter((person) => (person.name === name));
+    }
+
+    function findName(name) {
+        return people.find(person => person.name === name);
+    }
+    // map :: people => names
+    function allNames() {
+        return people.map(el => el.name)
+    }
+
+    function firstPersonCard() {
+        return `${people[0].name} is from:\n${people[0].address}`;
+    }
+
+};
+
